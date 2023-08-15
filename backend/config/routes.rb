0af941fbbe4f386a53336ee1c536678b750b6836
fujisang <86703276+fujisang@users.noreply.github.com>
2023-08-16 01:23:08 +0900

    Updated routes.rb
    /users/indexでデータベース内ユーザー表示＆/editで編集可。/updateは情報アップデート用（users_controller）。

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'users/index' => "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "users/:id/edit" => "users#edit"
+
+  post "users/:id/update" => "users#update"
 end
