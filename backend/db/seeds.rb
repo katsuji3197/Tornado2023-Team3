a75d5553ef948db177f88966c60a5b0f9ededd05
fujisang <86703276+fujisang@users.noreply.github.com>
2023-08-16 02:17:16 +0900

    サンプルデータ挿入

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name:"Ryo", email:"rryyoo52@gmail.com", sex:0, birthdate:"2004-01-17", hometown:"Tokyo", residence:"Kanagawa", job:"Engineer")
+User.create(name:"Thomas", email:"tthomas@gmail.com", sex:1, birthdate:"2000-12-20", hometown:"Hokkaido", residence:"US", job:"Baby")
