a256e275d89cf128bf3dc00c6e47f98dd0dc276a
fujisang <86703276+fujisang@users.noreply.github.com>
2023-08-27 23:44:47 +0900

    update:footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,6 +2,10 @@ import { useLocation, Link } from "react-router-dom";
 import myPageIcon from "../assets/images/icon_header_UnknownUser.svg";
 import accountSettingsIcon from "../assets/images/icon_header_UnknownUser.svg";
 import editProfileIcon from "../assets/images/icon_header_UnknownUser.svg";
+import wireIcon from "../assets/images/icon_footer_wire.svg";
+import recruitIcon from "../assets/images/icon_footer_rocket.svg";
+import chatIcon from "../assets/images/icon_footer_chat.svg";
+
 
 export const Footer = () => {
   const location = useLocation();
@@ -11,6 +15,24 @@ export const Footer = () => {
   return (
     <footer className="fixed bottom-0 w-full bg-indigo-950 py-2 shadow-top">
       <div className="flex justify-between px-5">
+        {/* 交信 */}
+        <Link
+          to="/match"
+          className={`flex flex-col items-center ${
+            isCurrentRoute("/match") ? "text-white" : "text-gray-400"
+          }`}
+        >
+          <img
+            src={wireIcon}
+            alt="Matching"
+            className={`w-${isCurrentRoute("/match") ? "8" : "6"} h-${
+              isCurrentRoute("/match") ? "8" : "6"
+            } mb-1`}
+          />
+          <span className="text-xs">交信</span>
+        </Link>
+        
+        {/* 募集 ※TODO:どこにリンクするかわからない*/}
         <Link
           to="/mypage"
           className={`flex flex-col items-center ${
@@ -18,49 +40,52 @@ export const Footer = () => {
           }`}
         >
           <img
+            src={recruitIcon}
+            alt="Recruit"
+            className={`w-${
+              isCurrentRoute("/mypage") ? "8" : "6"
+            } h-${
+              isCurrentRoute("/mypage") ? "8" : "6"
+            } mb-1`}
+          />
+          <span className="text-xs">募集</span>
+        </Link>
+        {/* チャット */}
+        <Link
+          to="/start-communication"
+          className={`flex flex-col items-center ${
+            isCurrentRoute("/start-communication") ? "text-white" : "text-gray-400"
+          }`}
+        >
+          <img
+            src={chatIcon}
+            alt="Chat"
+            className={`w-${
+              isCurrentRoute("/start-communication") ? "8" : "6"
+            } h-${
+              isCurrentRoute("/start-communication") ? "8" : "6"
+            } mb-1`}
+          />
+          <span className="text-xs">チャット</span>
+        </Link>
+
+        {/*My Page*/}
+        <Link
+          to="/edit-profile"
+          className={`flex flex-col items-center ${
+            isCurrentRoute("/mypage") ? "text-white" : "text-gray-400"
+          }`}
+        >
+          <img
             src={myPageIcon}
-            alt="My Page"
+            alt="Edit Profile"
             className={`w-${isCurrentRoute("/mypage") ? "8" : "6"} h-${
               isCurrentRoute("/mypage") ? "8" : "6"
-            } mb-1 bg-purple-500 rounded-full`}
+            } mb-1`}
           />
           <span className="text-xs">My Page</span>
         </Link>
-
-        <Link
-          to="/account-settings"
-          className={`flex flex-col items-center ${
-            isCurrentRoute("/account-settings") ? "text-white" : "text-gray-400"
-          }`}
-        >
-          <img
-            src={accountSettingsIcon}
-            alt="Account Settings"
-            className={`w-${
-              isCurrentRoute("/account-settings") ? "8" : "6"
-            } h-${
-              isCurrentRoute("/account-settings") ? "8" : "6"
-            } mb-1 bg-purple-500 rounded-full`}
-          />
-          <span className="text-xs">Account Settings</span>
-        </Link>
-
-        <Link
-          to={"/edit-profile"}
-          state={{ from: "/mypage" }}
-          className={`flex flex-col items-center ${
-            isCurrentRoute("/edit-profile") ? "text-white" : "text-gray-400"
-          }`}
-        >
-          <img
-            src={editProfileIcon}
-            alt="Edit Profile"
-            className={`w-${isCurrentRoute("/edit-profile") ? "8" : "6"} h-${
-              isCurrentRoute("/edit-profile") ? "8" : "6"
-            } mb-1 bg-purple-500 rounded-full`}
-          />
-          <span className="text-xs">Edit Profile</span>
-        </Link>
+        
       </div>
     </footer>
   );
