92602ec8df09cea8008a2e35a320e1d94641e1cb
yuporon <ftb.y.m.kashima.07@gmail.com>
2023-08-29 14:38:46 +0900

    feat: 軽微な修正

--- frontend/src/pages/users/DashboardPage.jsx
+++ frontend/src/pages/users/DashboardPage.jsx
@@ -9,22 +9,22 @@ const user = {
 
 const aliencount = 256;
 const alienEncounters = 200;
-const alienreview = 5;
-
-
+// const alienreview = 5;
 
 export const DashboardPage = () => {
   return (
-    <div className="dashboard-container p-8 bg-gray-50 text-gray-900 min-h-screen space-y-32">
-
+    <div className="dashboard-container p-8 bg-gray-50 text-gray-900 min-h-screen space-y-8">
       {/* ユーザーの写真と名前 */}
       <div className="user-info flex items-center space-x-4 mb-0">
         <img
           src={user.profilePicture}
           alt={`${user.name}'s profile`}
-          className="user-photo w-16 h-16 md:w-24 md:h-24 rounded-full border border-purple-500"/>
+          className="user-photo w-16 h-16 md:w-24 md:h-24 rounded-full border border-purple-500"
+        />
 
-        <h2 className="user-name text-xl md:text-2xl font-bold text-gray-900">{user.name}</h2>
+        <h2 className="user-name text-xl md:text-2xl font-bold text-gray-900">
+          {user.name}
+        </h2>
       </div>
 
       {/* 遭遇したエイリアンの数 */}
@@ -34,8 +34,7 @@ export const DashboardPage = () => {
           実績
           <div className="ml-2 w-full border border-gray-200 " />
         </h1>
-        <div className="flex justify-center">
-          
+        <div className="flex justify-between space-x-4">
           <div className="alien-encounters mb-4 bg-white p-6 rounded-xl shadow-xl">
             <p className="text-lg md:text-xl text-gray-900">
               遭遇した回数:{" "}
@@ -50,21 +49,20 @@ export const DashboardPage = () => {
           </div>
         </div>
       </div>
-      
 
       {/* プロフィール編集とアカウント設定への遷移ボタン */}
       <div>
-      {/* ページのタイトル */}
-      <h1 className="text-xl md:text-3xl font-bold mb-4 text-neutral-800">
+        {/* ページのタイトル */}
+        <h1 className="text-xl md:text-3xl font-bold mb-4 text-neutral-800">
           あなたについて
           <div className="ml-2 w-full border border-gray-200 " />
-      </h1>
+        </h1>
         <div className="navigation-buttons space-y-4">
           <Link
             to={"/edit-profile"}
             state={{ from: "/mypage" }}
             className={
-              "edit-profile-btn block block text-center font-bold text-gray-900 alien-encounters mb-4 bg-white p-6 rounded-xl shadow-xl hover:bg-gray-300"
+              "edit-profile-btn block text-center font-bold text-gray-900 alien-encounters mb-4 bg-white p-6 rounded-xl shadow-xl hover:bg-gray-300"
             }
           >
             プロフィール編集
@@ -77,10 +75,6 @@ export const DashboardPage = () => {
           </Link>
         </div>
       </div>
-
-      
     </div>
-
-    
   );
 };
