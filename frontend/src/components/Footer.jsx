1bf3c6ea10073bfaf108a54adb59d75692384dd4
yuporon <ftb.y.m.kashima.07@gmail.com>
2023-08-29 18:01:46 +0900

    fix: 仮URLの設定とテキストカラーの修正

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,93 +3,78 @@ import mypageIcon from "../assets/images/icon_footer_user.svg";
 import wireIcon from "../assets/images/icon_footer_wire.svg";
 import recruitIcon from "../assets/images/icon_footer_rocket.svg";
 import chatIcon from "../assets/images/icon_footer_chat.svg";
-{/* wireIconActive, recruitIconActive chatIconActive mypageIconActive */}
 import wireIconActive from "../assets/images/icon_footer_wire_colored.svg";
 import recruitIconActive from "../assets/images/icon_footer_rocket_colored.svg";
 import chatIconActive from "../assets/images/icon_footer_chat_colored.svg";
 import mypageIconActive from "../assets/images/icon_footer_user_colored.svg";
 
-
+import "../assets/styles/css/Footer.css";
 
 export const Footer = () => {
   const location = useLocation();
+
+  const MENU_ITEMS = [
+    {
+      to: "/start-communication",
+      defaultIcon: wireIcon,
+      activeIcon: wireIconActive,
+      alt: "Matching",
+      label: "交信",
+    },
+    {
+      // TODO: 仮のURL
+      to: "/account-settings",
+      defaultIcon: recruitIcon,
+      activeIcon: recruitIconActive,
+      alt: "Recruit",
+      label: "募集",
+    },
+    {
+      // TODO: 仮のURL
+      to: "/edit-profile",
+      defaultIcon: chatIcon,
+      activeIcon: chatIconActive,
+      alt: "Chat",
+      label: "チャット",
+    },
+    {
+      to: "/mypage",
+      defaultIcon: mypageIcon,
+      activeIcon: mypageIconActive,
+      alt: "Edit Profile",
+      label: "マイページ",
+    },
+  ];
 
   const isCurrentRoute = (route) => location.pathname === route;
 
   return (
     <footer className="fixed bottom-0 w-full bg-white py-2 shadow-top">
       <div className="flex justify-between px-5">
-        {/* 交信 */}
-        <Link
-          to="/match"
-          className={`flex flex-col items-center ${
-            isCurrentRoute("/match") ? "text-white" : "text-gray-400"
-          }`}
-        >
-          <img
-            src={isCurrentRoute("/match") ? wireIconActive : wireIcon}
-            alt="Matching"
-            className={`w-${isCurrentRoute("/match") ? "8" : "6"} h-${
-              isCurrentRoute("/match") ? "8" : "6"
-            } mb-1`}
-          />
-          <span className="text-xs">交信</span>
-        </Link>
-        
-        {/* 募集 ※TODO:どこにリンクするかわからない*/}
-        <Link
-          to="/"
-          className={`flex flex-col items-center ${
-            isCurrentRoute("/") ? "text-white" : "text-gray-400"
-          }`}
-        >
-          <img
-            src={isCurrentRoute("/") ? recruitIconActive : recruitIcon}            
-            alt="Recruit"
-            className={`w-${
-              isCurrentRoute("/") ? "8" : "6"
-            } h-${
-              isCurrentRoute("/") ? "8" : "6"
-            } mb-1`}
-          />
-          <span className="text-xs">募集</span>
-        </Link>
-        {/* チャット */}
-        <Link
-          to="/start-communication"
-          className={`flex flex-col items-center ${
-            isCurrentRoute("/start-communication") ? "text-white" : "text-gray-400"
-          }`}
-        >
-          <img
-            src={isCurrentRoute("/start-communication") ? chatIconActive : chatIcon}
-            alt="Chat"
-            className={`w-${
-              isCurrentRoute("/start-communication") ? "8" : "6"
-            } h-${
-              isCurrentRoute("/start-communication") ? "8" : "6"
-            } mb-1`}
-          />
-          <span className="text-xs">チャット</span>
-        </Link>
-
-        {/*My Page*/}
-        <Link
-          to="/mypage"
-          className={`flex flex-col items-center ${
-            isCurrentRoute("/mypage") ? "text-white" : "text-gray-400"
-          }`}
-        >
-          <img
-            src={isCurrentRoute("/mypage") ? mypageIconActive : mypageIcon}
-            alt="Edit Profile"
-            className={`w-${isCurrentRoute("/mypage") ? "8" : "6"} h-${
-              isCurrentRoute("/mypage") ? "8" : "6"
-            } mb-1`}
-          />
-          <span className="text-xs">My Page</span>
-        </Link>
-        
+        {MENU_ITEMS.map((item) => (
+          <Link
+            key={item.to}
+            to={item.to}
+            className={`flex flex-col items-center ${
+              isCurrentRoute(item.to) ? "text-white" : "text-gray-400"
+            }`}
+          >
+            <img
+              src={isCurrentRoute(item.to) ? item.activeIcon : item.defaultIcon}
+              alt={item.alt}
+              className="w-8 h-8"
+            />
+            <span
+              className={
+                isCurrentRoute(item.to)
+                  ? "text-xs font-bold footer-label-active"
+                  : "text-xs text-gray-500"
+              }
+            >
+              {item.label}
+            </span>
+          </Link>
+        ))}
       </div>
     </footer>
   );
