a12444797ddd845e8d8e5f578a74e81a24b77d4f
yuporon <ftb.y.m.kashima.07@gmail.com>
2023-09-05 16:14:38 +0900

    feat: matchapiの受け取り

--- frontend/src/api/matches.js
+++ frontend/src/api/matches.js
@@ -1,5 +1,20 @@
 import client from "./client";
+import Cookies from "js-cookie";
 
+// 認証に関するヘッダーを取得
+const getAuthHeaders = () => {
+  const headers = {
+    "access-token": Cookies.get("_access_token"),
+    client: Cookies.get("_client"),
+    uid: Cookies.get("_uid"),
+  };
+
+  return headers;
+};
+
+// マッチを作成または探すAPI
 export const findMatch = (params) => {
-  return client.post("matches/find_match", params);
+  return client.post("matches/find_match", params, {
+    headers: getAuthHeaders(),
+  });
 };
