79a8136445aada852390bd582fa198c7dfe39614
yuporon <ftb.y.m.kashima.07@gmail.com>
2023-09-05 17:58:43 +0900

    feat: apiの修正

--- backend/app/controllers/api/v1/matched_users_controller.rb
+++ backend/app/controllers/api/v1/matched_users_controller.rb
@@ -1,5 +1,19 @@
-class Api::V1::MachedUsersController < ApplicationController
+class Api::V1::MatchedUsersController < ApplicationController
   before_action :set_current_user
+
+  # 指定した日時と場所のマッチングを持つユーザーを全て取得する
+  def index
+    # matchIDはリクエストで来る dateとmeet_locationから取得する
+    # MatchedUserのmatch_idが指定のmatch_idと一致するものを全て取得し、そのユーザーずを取得する
+    match_id = Match.find_by(match_date: match_params[:match_date], meet_location: match_params[:meet_location])
+    matched_users = MatchedUser.where(match_id: match_id).map(&:user)
+    if matched_users
+      matched_users.
+      render json: { status: 200, matched_users: matched_users }
+    else
+      render json: { status: 404, error: 'Matched user not found' }
+    end
+  end
 
   def create
     @matched_user = MatchedUser.find_or_initialize_by(user: @current_user, match_id: matched_user_params[:match_id])
@@ -29,5 +43,9 @@ class Api::V1::MachedUsersController < ApplicationController
 
   def matched_user_params
     params.require(:matched_user).permit(:match_id, :status)
+  end
+
+  def match_params
+    params.require(:matched_user).permit(:match_date, :meet_location)
   end
 end
--- backend/app/controllers/api/v1/matches_controller.rb
+++ backend/app/controllers/api/v1/matches_controller.rb
@@ -1,11 +1,16 @@
+class Api::V1::MatchesController < ApplicationController
   before_action :set_current_user, only: [:find_match]
 
   def find_match
-    # ログイン中のユーザーがすでにマッチングを申し込んでいる場合は既存のマッチを探します
+    # 分単位だとマッチングができないので、
+    # ここで一旦match_dateを1時間単位に変換
+    # * 例: 2021-09-01 12:30:00 => 2021-09-01 12:00:00
+    match_date = Time.zone.parse(match_params[:match_date]).beginning_of_hour
+    # ログイン中のユーザーがすでにマッチングを申し込んでいる場合fは既存のマッチを探します
     current_user_match = @current_user.matched_users
                                       .where(status: 'pending')
                                       .joins(:match)
-                                      .find_by(matches: { match_date: match_params[:match_date], meet_location: match_params[:meet_location] })
+                                      .find_by(matches: { match_date: match_date, meet_location: match_params[:meet_location] })
     if current_user_match
       match = current_user_match.match
     else
@@ -17,7 +22,8 @@
   end
 
   def create
-    match = Match.new(match_params)
+    match_date = Time.zone.parse(match_params[:match_date]).beginning_of_hour
+    match = Match.new(match_date: match_date, meet_location: match_params[:meet_location])
     if match.save
       render json: { status: 200, match: match }
     else
@@ -32,13 +38,14 @@
   end
 
   def find_or_create_match
+    match_date = Time.zone.parse(match_params[:match_date]).beginning_of_hour
     match = Match.left_outer_joins(:matched_users)
                   .group('matches.id')
                   .having('COUNT(matched_users.id) < 6')
-                  .where(match_date: match_params[:match_date], meet_location: match_params[:meet_location])
+                  .where(match_date: match_date, meet_location: match_params[:meet_location])
                   .first
     # 既存のマッチが見つからなかった場合、新しいマッチを作成
-    match || Match.create(match_params)
+    match || Match.create(match_date: match_date, meet_location: match_params[:meet_location])
   end
 
 
